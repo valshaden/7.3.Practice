959547270166a7711eb0aeee7474aed98db6bb9c
valshaden <valshaden@yandex.ru>
2025-08-23 11:16:48 +0300

    + Создаем проект с интерфейсом

--- 1. We get currency exchange rates from the Internet.py
+++ 1. We get currency exchange rates from the Internet.py
@@ -18,9 +18,8 @@ import requests
 import json
 import pprint
 
-# настраиваем PrettyPrinter
-
 result = requests.get('https://open.er-api.com/v6/latest/USD')
 data = json.loads(result.text)
+# настраиваем PrettyPrinter
 p = pprint.PrettyPrinter(indent=4)  # indent=4 - значит отступ в 4 пробела
 p.pprint(data)  # "красиво" выводим на экран данные
