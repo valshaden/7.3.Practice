32ba78e63b4e4ad2e4836bc29ba3e9102f8c98c6
valshaden <valshaden@yandex.ru>
2025-08-23 11:09:07 +0300

    + Получаем курсы валют из интернета

--- 1. We get currency exchange rates from the Internet.py
+++ 1. We get currency exchange rates from the Internet.py
@@ -4,3 +4,23 @@
 # 7.3.4. Генерация изображений
 
 # 1. Получаем курсы валют из интернета
+# https://open.er-api.com/v6/latest/USD
+
+##import requests
+##import json
+##result = requests.get('https://open.er-api.com/v6/latest/USD')
+##data = json.loads(result.text)
+##print(data)
+
+# Выводим красиво с помощью PrettyPrinter
+
+import requests
+import json
+import pprint
+
+# настраиваем PrettyPrinter
+
+result = requests.get('https://open.er-api.com/v6/latest/USD')
+data = json.loads(result.text)
+p = pprint.PrettyPrinter(indent=4)  # indent=4 - значит отступ в 4 пробела
+p.pprint(data)  # "красиво" выводим на экран данные
